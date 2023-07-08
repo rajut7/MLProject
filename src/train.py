ea5055ab67805542283211eb8fa9c0400866ea70
rajut7 <tadisettiraju72@gmail.com>
2023-07-07 23:05:50 -0500

    modified

--- src/train.py
+++ src/train.py
@@ -111,7 +111,7 @@ def compute_metrics_by_slice(
             X_train, X_test, y_train, y_test = train_test_split(
                 X_slice, y_slice, test_size=0.2, random_state=42)
             model = train_model(X_train, y_train)
-            # Perform model inference on the slice
+            # Perform mdel inference
             y_pred = inference(model, X_test)
 
             # Compute performance metrics for the slice
